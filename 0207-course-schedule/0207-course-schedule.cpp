005394057a1c12274ec050d4af909a4d526d74ed
viru-ydv7 <166306732+viru-ydv7@users.noreply.github.com>
2026-07-04 11:33:51 +0530

    Time: 0 ms (100%), Space: 19.4 MB (48.67%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,31 +1,76 @@
+
+// class Solution {
+// private:
+//     bool dfs(int node , vector<int>&visited , vector<int>&path , vector<vector<int>> &adj){
+//         visited[node]=1;
+//         path[node]=1;
+
+//         for(int i=0;i<adj[node].size();i++){
+//             int neighbour = adj[node][i];
+//             if(visited[neighbour]){
+//                 if(path[neighbour]){
+//                     return true;
+//                 }
+//             }
+//             else{
+//                 if(dfs(neighbour,visited,path,adj)==true){
+//                     return true;
+//                 }
+//             }
+//         }
+//         path[node]=0;
+//         return false;
+
+//     }
+// public:
+//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+//         vector<vector<int>>adj(numCourses);
+//         vector<int>path(numCourses,0);
+//         vector<int>visited(numCourses,0);
+//         for(auto &e:prerequisites){
+//             adj[e[1]].push_back(e[0]);
+//         }
+
+//         for(int i=0;i<numCourses;i++){
+//             if(visited[i]!=1){
+//                 if(dfs(i,visited,path,adj)==true){
+//                     return false;
+//                 }
+//             }
+            
+//         }
+//         return true;
+//     }
+// };
+
+
 class Solution {
 private:
-    bool dfs(int node , vector<int>&visited , vector<int>&path , vector<vector<int>> &adj){
+    bool dfs(int node ,vector<int>&visited,vector<int>&path, vector<vector<int>>&adj){
         visited[node]=1;
         path[node]=1;
-
         for(int i=0;i<adj[node].size();i++){
-            int neighbour = adj[node][i];
-            if(visited[neighbour]){
-                if(path[neighbour]){
+            int n=adj[node][i];
+            if(visited[n]){
+                if(path[n]){
                     return true;
                 }
             }
             else{
-                if(dfs(neighbour,visited,path,adj)==true){
+                if(dfs(n,visited,path,adj)==true){
                     return true;
                 }
             }
         }
         path[node]=0;
         return false;
-
     }
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>>adj(numCourses);
         vector<int>path(numCourses,0);
         vector<int>visited(numCourses,0);
+
         for(auto &e:prerequisites){
             adj[e[1]].push_back(e[0]);
         }
