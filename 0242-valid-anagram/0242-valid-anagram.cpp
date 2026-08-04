310e1bb1abf49f6620e6c81810a8fa5696d1d561
viru-ydv7 <166306732+viru-ydv7@users.noreply.github.com>
2026-08-04 20:37:48 +0530

    Time: 0 ms (100%), Space: 9.8 MB (50.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,51 +1,51 @@
-// class Solution {
-// public:
-//     bool isAnagram(string s, string t) {
-//         vector<int>a(26,0);
-//         vector<int>b(26,0);
-
-//         for(int i=0;i<s.length();i++){
-//             int index=s[i]-'a';
-//             a[index]++;
-//         }
-//         for(int i=0;i<t.length();i++){
-//             int index=t[i]-'a';
-//             b[index]++;
-//         }
-
-//         for(int i=0;i<26;i++){
-//             if(a[i]!=b[i]){
-//                 return false;
-//             }
-//         }
-//         return true;
-//     }
-// };
-
-
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector<int>alpha(26,0);
-        int n=s.length();
-        int m=t.length();
+        vector<int>a(26,0);
+        vector<int>b(26,0);
 
-        if(n!=m){
-            return false;
-        }
         for(int i=0;i<s.length();i++){
-            int index = s[i]-'a';
-            int index2 = t[i]-'a';
-
-            alpha[index]++;
-            alpha[index2]--;
+            int index=s[i]-'a';
+            a[index]++;
+        }
+        for(int i=0;i<t.length();i++){
+            int index=t[i]-'a';
+            b[index]++;
         }
 
-        for(int e:alpha){
-            if(e!=0){
+        for(int i=0;i<26;i++){
+            if(a[i]!=b[i]){
                 return false;
             }
         }
         return true;
     }
 };
+
+
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         vector<int>alpha(26,0);
+//         int n=s.length();
+//         int m=t.length();
+
+//         if(n!=m){
+//             return false;
+//         }
+//         for(int i=0;i<s.length();i++){
+//             int index = s[i]-'a';
+//             int index2 = t[i]-'a';
+
+//             alpha[index]++;
+//             alpha[index2]--;
+//         }
+
+//         for(int e:alpha){
+//             if(e!=0){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
