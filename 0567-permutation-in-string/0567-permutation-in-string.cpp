b8155f035c30fcc2efb3d10afbf4e83f8f867046
viru-ydv7 <166306732+viru-ydv7@users.noreply.github.com>
2026-08-13 19:52:12 +0530

    Time: 0 ms (100%), Space: 10.1 MB (45.12%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {;
-        
+        if(s1.length() > s2.length()) return false;
         int windowsize=s1.length();
 
         vector<int>hash1(26,0);
+        
         for(int i=0;i<s1.length();i++){
             hash1[s1[i]-'a']++;
+        }
+
+        vector<int>hash2(26,0);
+        for(int i=0;i<windowsize;i++){
+            hash2[s2[i]-'a']++;
         }
 
 
@@ -14,25 +20,26 @@ public:
         int j=windowsize-1;
         int n=s2.length();
         
-        while(j<n){
-           vector<int>hash2(hash1);
-           
-           for(int k=i;k<=j;k++){
-            hash2[s2[k]-'a']--;
-           }
+        while(j<n){           
             int flag=true;
-           for(int k=0;k<26;k++){
-                if(hash2[k]!=0){
-                    i++;
-                    j++;
-                    flag=false;
-                    break;
-                }
-           }
+            for(int k=0;k<26;k++){
+                    if(hash2[k]!=hash1[k]){
+                        if(j==n-1){
+                            return false;
+                        }
+                        hash2[s2[i]-'a']--;
+                        hash2[s2[j+1]-'a']++;
+                        i++;
+                        j++;
+                        flag=false;
+                        break;
+                    }
+            }
 
-           if(flag==true){
-            return true;
-           }
+            if(flag==true){
+                return true;
+            }
+            
 
            
         }
