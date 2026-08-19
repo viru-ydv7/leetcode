c7695e8c37366dc318fcfac38af8f6601e6680e6
viru-ydv7 <166306732+viru-ydv7@users.noreply.github.com>
2026-08-19 20:01:08 +0530

    Time: 0 ms (100%), Space: 31.4 MB (7.6%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l=0;
+        int r=nums.size();
+        while(l<r){
+            int mid= l+(r-l)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+              r=mid;
+            }
+            else{
+                l=mid+1;
+            }
+
+        }
+        return -1;
+
+
+    }
+};
