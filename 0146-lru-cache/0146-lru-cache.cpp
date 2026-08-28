8914b754a03eeea3ba9aed926b95de5b927db290
viru-ydv7 <166306732+viru-ydv7@users.noreply.github.com>
2026-08-28 13:58:19 +0530

    Time: 11 ms (96.94%), Space: 48 MB (92.48%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,90 +1,85 @@
 class LRUCache {
 public:
+    class Node
+    {
+        public:
+            int key;
+            int value;
+            Node *prev;
+            Node  *next;
 
-    class Node {
-    public:
-        int key;
-        int value;
-        Node* next;
-        Node* prev;
-
-        Node(int k, int v) {
-            key = k;
-            value = v;
-            next = NULL;
-            prev = NULL;
-        }
+            Node(int k,int value){
+                key=k;
+                this->value=value;
+                prev=NULL;
+                next=NULL;
+            }
     };
-
     int capacity;
-    unordered_map<int, Node*> mp;
-
-    Node* head;
-    Node* tail;
+    unordered_map<int,Node*>mp;
+    Node *head;
+    Node *tail;
 
     LRUCache(int capacity) {
-        this->capacity = capacity;
+        this->capacity=capacity;
+        head = new Node(-1,-1);
+        tail=new Node (-1,-1);
 
-        head = new Node(-1, -1);
-        tail = new Node(-1, -1);
-
-        head->next = tail;
-        tail->prev = head;
+        head->next=tail;
+        tail->prev=head;
     }
-
-    void remove(Node* node) {
-        Node* previous = node->prev;
-        Node* following = node->next;
-
-        previous->next = following;
-        following->prev = previous;
+    void insert(Node*node){
+        Node *last=tail->prev;
+        last->next=node;
+        node->prev=last;
+        
+        node->next=tail;
+        tail->prev=node;
     }
-
-    void insert(Node* node) {
-        Node* last = tail->prev;
-
-        last->next = node;
-        node->prev = last;
-
-        node->next = tail;
-        tail->prev = node;
+    void remove(Node*node){
+        Node *pichla=node->prev;
+        Node *agla=node->next;
+        pichla->next=agla;
+        agla->prev=pichla;
     }
-
     int get(int key) {
-        if(mp.find(key) == mp.end()) {
+        if(mp.find(key)==mp.end()){
             return -1;
         }
-
-        Node* node = mp[key];
-
+        Node *node=mp[key];
         remove(node);
         insert(node);
-
         return node->value;
     }
-
+    
     void put(int key, int value) {
-
-        if(mp.find(key) != mp.end()) {
-            Node* node = mp[key];
-
-            node->value = value;
+        if(mp.find(key)!=mp.end()){
+            Node *node=mp[key];
+            node->value=value;
 
             remove(node);
             insert(node);
         }
-        else {
-            Node* newnode = new Node(key, value);
+        else{
+            Node *newnode=new Node(key,value);
 
-            mp[key] = newnode;
+            mp[key]=newnode;
             insert(newnode);
-
-            if(mp.size() > capacity) {
+            if(mp.size()>capacity){
                 Node* lru = head->next;
 
                 remove(lru);
                 mp.erase(lru->key);
             }
+
+
         }
     }
 };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
