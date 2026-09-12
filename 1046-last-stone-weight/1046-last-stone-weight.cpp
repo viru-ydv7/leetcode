0681f70082010b390547155dd7b10fd20d156c40
viru-ydv7 <166306732+viru-ydv7@users.noreply.github.com>
2026-09-12 20:48:35 +0530

    Time: 0 ms (100%), Space: 10.1 MB (12.44%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>q;
+        for(int i=0;i<stones.size();i++){
+            q.push(stones[i]);
+        }
+
+        while(q.size()!=1){
+            int a=q.top();
+            q.pop();
+            int b=q.top();
+            q.pop();
+
+            int c;
+            if(a==b){
+                c=0;
+            }
+            else{
+                c=abs(a-b);
+            }
+            q.push(c);
+        }
+
+        int ans=q.top();
+        return ans;
+    }
+};
